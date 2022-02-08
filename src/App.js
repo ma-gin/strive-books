35257d42646f72f74ae021f8d8e3d4f3263a910d
Matt Ginter <mattginter@gmail.com>
2022-02-08 15:00:52 +0100

    add jumbotron

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import MyFooter from './components/MyFooter'
+import MyJumbotron from './components/MyJumbotron'
 import MyNav from "./components/MyNav";
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <MyNav bookstore="Strive Books"></MyNav>
+      <MyJumbotron></MyJumbotron>
       <MyFooter></MyFooter>
     </div>
   );
